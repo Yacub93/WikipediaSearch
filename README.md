06b6b7881e740451acd082a3b6b4d6e67f15d1b6
Yacub Ali <Yacub93@Yacubs-MacBook-Pro.local>
2017-05-30 19:04:30 +0100

    App displayes images and reset feature added

---
+++ README.md
@@ -0,0 +1,22 @@
+# Wikipedia Search App
+
+A simple, wikipedia search app built using the AngularJS Framework. It gathers data from Wikipedia, via the Wikipedia API: https://www.mediawiki.org/wiki/API:Main_page. 
+
+
+## Features:
+ * Search Feature for entering search terms, entries displayed below.
+ * View full Wikipedia entry by clicking the link.
+ * Clear search field and results by clicking the reset button.
+
+## App Screenshot
+<br>
+* Version 1
+<img width="1280" alt="screen shot 2017-05-30 at 18 59 24" src="https://cloud.githubusercontent.com/assets/26364425/26597493/1f904e5a-456a-11e7-8ea8-674f5c2415b2.png">
+<br>
+
+## TODO:
+
+* Add View more entries feature
+
+
+
--- js/wikiviewer.js
+++ js/wikiviewer.js
@@ -11,36 +11,49 @@ classApp.controller("wikiCtrl", function($scope, $http) {
     }
   };
 
+  $scope.results = [];
+  console.log($scope.results);
 
-$scope.results = [];
+      $scope.reset = function() {
+        if($scope.formData.searchTerm) $scope.formData.searchTerm = '';
+        if($scope.results) $scope.results = '';
+    };
 
-  $scope.search = function() {
+  	// Check search field before proceeding
+  	$scope.check = function() {
+        if ($scope.formData.searchTerm === "") {
+        	return false;
+        }
+        return true;
+    }
+
+  $scope.search = function() { 
     $scope.results = [];
+    console.log($scope.results + " Search function");
     var title = $scope.formData.searchTerm;
     var wikiURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
     var cb = '&callback=JSON_CALLBACK';
     var page = 'https://en.wikipedia.org/?curid=';
     
-    // use jsonp to bypass the HTTP access control (cors)
+    // use jsonp for cross-site requests to bypass the HTTP access control (CORS)
     $http.jsonp(wikiURL + title + cb)
     .success(function(data) {
     	// console.log(data);
       var results = data.query.pages;
       // console.log(results);
-      	
 
         angular.forEach(results, function(value, key) {
         $scope.results.push({title: value.title, 
         					 body: value.extract,
-        					 page: page + value.pageid
+        					 page: page + value.pageid,
+        					 image: value.thumbnail.source
         					 });
 
-        // console.log("value= " + v + "key= " + k);
         console.log($scope.results);
-      });
-    });
-  }
-
+    	  });
+    	});
+      } //.Close search function
+    //} //.Close check function
 }); //.Close controller         
     
 
