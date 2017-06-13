79ce81a78663d3b74ab45269f8b1feebd504e526
Yacub Ali <Yacub93@Yacubs-MacBook-Pro.local>
2017-06-13 20:01:45 +0100

    Validation Added

--- js/wikiviewer.js
+++ js/wikiviewer.js
@@ -1,7 +1,7 @@
 var classApp = angular.module('wikiApp', []);
 
 classApp.controller("wikiCtrl", function($scope, $http) {
-
+ // angular.element(document).ready(function () {  
   $scope.wikiInfo = {
     heading: "Wikipedia Search",
     appInfo: "A simple wikipedia app that displays that lets you search information using the search input.",
@@ -11,49 +11,54 @@ classApp.controller("wikiCtrl", function($scope, $http) {
     }
   };
 
+
   $scope.results = [];
-  console.log($scope.results);
 
       $scope.reset = function() {
         if($scope.formData.searchTerm) $scope.formData.searchTerm = '';
         if($scope.results) $scope.results = '';
     };
 
-  	// Check search field before proceeding
-  	$scope.check = function() {
-        if ($scope.formData.searchTerm === "") {
-        	return false;
-        }
-        return true;
-    }
 
-  $scope.search = function() { 
+  $scope.search = function(value) {
+    if (typeof value == 'undefined' || value == null || value == "") {
+      console.log("searchTerm " + value);
+      alert("Please Enter a Search Term!");
+    } 
+    else {
+
     $scope.results = [];
-    console.log($scope.results + " Search function");
+
     var title = $scope.formData.searchTerm;
     var wikiURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=20&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
     var cb = '&callback=JSON_CALLBACK';
     var page = 'https://en.wikipedia.org/?curid=';
-    
-    // use jsonp for cross-site requests to bypass the HTTP access control (CORS)
+
+   
+    // Use jsonp for cross-site requests to bypass the HTTP access control (CORS)
     $http.jsonp(wikiURL + title + cb)
     .success(function(data) {
-    	// console.log(data);
+      // if (typeof data == 'undefined' || data == null) {
+      // console.log(data);
+      // alert("No Search results found!");
+    // } 
       var results = data.query.pages;
-      // console.log(results);
 
         angular.forEach(results, function(value, key) {
         $scope.results.push({title: value.title, 
         					 body: value.extract,
         					 page: page + value.pageid,
-        					 image: value.thumbnail.source
+        					 // image: value.thumbnail.source
         					 });
 
-        console.log($scope.results);
+        // console.log($scope.results);
+
     	  });
     	});
-      } //.Close search function
-    //} //.Close check function
+    } //.Close check searchTerm
+  } //.Close search function
+  // });//document.ready
+
 }); //.Close controller         
     
 
